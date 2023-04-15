dc6aae476b88133e4ef95a0627f4a2a754a8afec
Satish Venkatakrishnan <satish860@gmail.com>
2023-04-15 17:46:30 +0530

    Changed the pages and verified for Multiple tones

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html data-theme="garden" lang="en">
-      <Head />
+      <Head title="Quillbot Clone" />
       <body>
         <Main />
         <NextScript />
