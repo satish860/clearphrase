f30cc7ea955b2ab807edb93999b4beace2754ffd
Satish Venkatakrishnan <satish860@gmail.com>
2023-04-15 07:24:42 +0530

    Added Top Nav

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="bg-gray-600">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
