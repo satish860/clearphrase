5c8c0e6063f68fb42b70dd3d493eb5a75ae8da01
apprakash <101494351+apprakash@users.noreply.github.com>
2023-05-09 18:15:22 +0530

    Header changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,21 +10,8 @@ import Header from "./components/Header";
 export default function Landing() {
   return (
     <>
-      <Flex bg="white" w="100%" p={4} verticalAlign="center">
-        <Text fontSize="3xl">Clear</Text>
-        <Text fontSize="3xl" color="green.600" transform={"rotate(-20deg)"}>
-          P
-        </Text>
-        <Text fontSize="3xl" color="green.600">
-          hrase
-        </Text>
-        <Text fontSize="3xl" color="black">
-          .io
-        </Text>
-        <Header />
-      </Flex>
-
-      <CallToActionWithAnnotation />
+      <Header />
+     <CallToActionWithAnnotation />
       <SimpleThreeColumns />
       <WithSpeechBubbles />
       <SmallWithSocial />
