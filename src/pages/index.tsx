81bf36ed0bcb81aecc9d463ce56a81bd44593ba5
apprakash <101494351+apprakash@users.noreply.github.com>
2023-05-09 11:36:27 +0530

    Sign In button - Home Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,13 @@ import CallToActionWithAnnotation from "./components/CallToActionWithAnnotation"
 import SimpleThreeColumns from "./components/Features";
 import WithSpeechBubbles from "./components/Testimonials";
 import SmallWithSocial from "./components/Footer";
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Text, Button, Stack, Box } from "@chakra-ui/react";
 
 export default function Landing() {
   return (
     <>
       <Flex bg="white" w="100%" p={4} verticalAlign="center">
-        <Text fontSize="3xl">
-          Clear
-        </Text>
+        <Text fontSize="3xl">Clear</Text>
         <Text fontSize="3xl" color="green.600" transform={"rotate(-20deg)"}>
           P
         </Text>
@@ -20,6 +18,21 @@ export default function Landing() {
         <Text fontSize="3xl" color="black">
           .io
         </Text>
+        <Stack
+         direction="row" ml="auto" alignItems="center"
+        >
+          <Button
+            colorScheme={"green"}
+            bg={"green.400"}
+            rounded={"full"}
+            px={6}
+            _hover={{
+              bg: "green.500",
+            }}
+          >
+            Sign In
+          </Button>
+        </Stack>
       </Flex>
 
       <CallToActionWithAnnotation />
