1b36b7df31f56c8a49f3f49016609d6308ea6235
apprakash <101494351+apprakash@users.noreply.github.com>
2023-05-09 11:52:33 +0530

    Sticky Header

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -3,7 +3,7 @@ import { Flex, Text, Button, Stack } from "@chakra-ui/react";
 export default function Header() {
   return (
     <>
-      <Flex bg="white" w="100%" p={4} verticalAlign="center">
+      <Flex bg="white" w="100%" p={4} verticalAlign="center" as="header" position="fixed">
         <Text fontSize="3xl">Clear</Text>
         <Text fontSize="3xl" color="green.600" transform={"rotate(-20deg)"}>
           P
